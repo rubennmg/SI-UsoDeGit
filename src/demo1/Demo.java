e23e54ce1416d67c46b7f18e35704854e47c23b8
ruben <ruben@rubennmg-asus>
2023-02-12 14:40:37 +0100

    cambios en Demo.java

--- src/demo1/Demo.java
+++ src/demo1/Demo.java
@@ -2,4 +2,9 @@ package demo1;
 
 public class Demo {
 	
+	public static void main(String[] args) {
+		System.out.println("Línea 1");
+		System.out.println("Línea 2");
+		System.out.println("Línea 3");
+	}
 }
